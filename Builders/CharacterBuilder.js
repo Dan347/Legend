f63feb83e815c5b8932c9beb4a0beb710820f4fb
Dan347 <deoshea@gmail.com>
2014-08-24 09:27:01 -0400

    Building second layer
    Class templates added. Created Builder and Factory folders. Also
    templated one factory and one builder object.

---
+++ Builders/CharacterBuilder.js
@@ -0,0 +1,29 @@
+/*
+This builds a character object.
+I need factories inside of here.
+1. Build Character
+2. Assign Skill
+3. ????
+4. Profit
+*/
+"use strict";
+
+
+
+function CharacterBuilder()
+{
+
+
+}
+
+function buildCharacter()
+{
+}
+
+function setBasicSkills()
+{
+}
+
+function buildAdvancedSkills()
+{
+}
