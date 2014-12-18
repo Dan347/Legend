a38bbd629c3025a2aa0c2a5d430f1c54ba78dbd1
Dan347 <deoshea@gmail.com>
2014-12-18 17:38:23 -0500

    commit remaining grunt files

--- Builders/CharacterBuilder.js
+++ Builders/CharacterBuilder.js
@@ -6,34 +6,34 @@ I need factories inside of here.
 3. ????
 4. Profit
 */
-"use strict";
 
 
 //first build attributes
 //then build character
 function CharacterBuilder()
 {
-  return this;
+    "use strict";
+    function buildCharacter(name, strength, dexterity, constitution, size, intelligence, power, charisma, advancedSkills, basicSkills)
+    {
+        var attributes = new Attributes(strength, dexterity, constitution, size, intelligence, power, charisma);
+        var basicSkills = setBasicSkills(attributes);
+        var advancedSkills = setAdvancedSkills(advancedSkillNames, attributes)
+        var char = new Character(name, attributes, advancedSkills, basicSkills);
 
-}
+        return char;
+    }
+
+    function setBasicSkills(attributes)
+    {
+    }
+
+    function buildAdvancedSkills(character, attributes)
+    {
+
+    }
 
 
 
-function buildCharacter(name, strength, dexterity, constitution, size, intelligence, power, charisma, advancedSkills, basicSkills)
-{
-  var attributes = new Attributes(strength, dexterity, constitution, size, intelligence, power, charisma);
-  var basicSkills = setBasicSkills(attributes);
-  var advancedSkills = setAdvancedSkills(advancedSkillNames, attributes)
-  var char = new Character(name, attributes, advancedSkills, basicSkills);
 
-  return char;
-}
-
-function setBasicSkills(attributes)
-{
-}
-
-function buildAdvancedSkills(character, attributes)
-{
-
+    return this;
 }
